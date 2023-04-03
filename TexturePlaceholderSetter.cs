f9981ca8d7dcfd1204d7ef3a392364cba0f8123e
Bryan Bortz <owlboy@owlboy.com>
2023-04-03 16:54:33 -0700

    🔨
    * Does proper checks for the editor
    * Does not block compiling with the VRCSDK
    * Simplified
    * No more warnings

--- TexturePlaceholderSetter.cs
+++ TexturePlaceholderSetter.cs
@@ -1,18 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 
 public class TexturePlaceholderSetter : MonoBehaviour
 {
     public Material targetMaterial;
     public Texture mainTexture;
-}
 
-[InitializeOnLoad]
-public class TextureSetterEditor
-{
-    static TextureSetterEditor()
+#if UNITY_EDITOR
+    [InitializeOnLoadMethod]
+    private static void Initialize()
     {
         EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
     }
@@ -21,14 +19,14 @@ public class TextureSetterEditor
     {
         if (state == PlayModeStateChange.ExitingPlayMode)
         {
-            EditorApplication.delayCall += SetMainTextures;
+            SetMainTextures();
         }
     }
+#endif
 
     private static void SetMainTextures()
     {
-        TexturePlaceholderSetter[] textureSetters = GameObject.FindObjectsOfType<TexturePlaceholderSetter>();
-        foreach (TexturePlaceholderSetter textureSetter in textureSetters)
+        foreach (TexturePlaceholderSetter textureSetter in FindObjectsOfType<TexturePlaceholderSetter>())
         {
             SetMainTexture(textureSetter);
         }
@@ -36,13 +34,16 @@ public class TextureSetterEditor
 
     private static void SetMainTexture(TexturePlaceholderSetter textureSetter)
     {
-        if (textureSetter.targetMaterial == null || textureSetter.mainTexture == null)
+        if (textureSetter.targetMaterial && textureSetter.mainTexture)
+        {
+            textureSetter.targetMaterial.SetTexture("_MainTex", textureSetter.mainTexture);
+#if UNITY_EDITOR
+            EditorUtility.SetDirty(textureSetter.targetMaterial);
+#endif
+        }
+        else
         {
             Debug.LogWarning("Target Material or Main Texture is not assigned.");
-            return;
         }
-
-        textureSetter.targetMaterial.SetTexture("_MainTex", textureSetter.mainTexture);
-        EditorUtility.SetDirty(textureSetter.targetMaterial);
     }
 }
